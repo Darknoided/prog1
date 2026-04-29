170b356fd88f1de259ec013a55761dc94a7e9d16
Your Name <you@example.com>
2026-04-29 15:11:09 -0300

    Hoje criamos ToDo simples em js

---
+++ public/main.js
@@ -0,0 +1,24 @@
+console.log("sdakjhasdkj");
+
+
+const containerAdicionar = document.querySelector('.adicionar')
+const inputAdicionar = document.querySelector('input')
+const btAdicionar = document.querySelector('button')
+const containerTarefas = document.querySelector('.tarefas')
+const templateTarefa = document.querySelector('template')
+
+function criarTarefa(texto) {
+    if (texto.trim() === '') return
+    const tarefa = templateTarefa.content.cloneNode(true)
+    const spanTitle = tarefa.querySelector('span')
+    const btExcluir = tarefa.querySelector('button')
+    spanTitle.textContent = texto
+    containerTarefas.appendChild(tarefa)
+    btExcluir.onclick = () => btExcluir.closest('.tarefa').remove()
+}
+
+btAdicionar.onclick = function () {
+    const texto = inputAdicionar.value.trim()
+    criarTarefa(texto)
+    inputAdicionar.value = ''
+}
